ab2ba9ec0ac7999dc3df5f569d53d4681172d38d
Alexander Gamaunov <unlike777@gmail.com>
2015-03-19 16:59:24 +0600

    Переработал вывод меню в админке
    добавлены группы

--- app/composers.php
+++ app/composers.php
@@ -2,9 +2,19 @@
 
 View::composer('admin.widgets.mainMenu.*', function($view) {
 	$menu = array(
-		array('name' => 'Страницы', 'url' => '/admin', 'route' => 'PagesAdminController', 'glyph' => 'glyphicon-th-list'),
-		array('name' => 'Пользователи', 'url' => '/admin/users', 'route' => 'UsersAdminController', 'glyph' => 'glyphicon-user'),
-		array('name' => 'Настройки', 'url' => '/admin/settings', 'route' => 'SettingsAdminController', 'glyph' => 'glyphicon-wrench'),
+		array(
+			'group' => 'Структура',
+			'items' => array(
+				array('name' => 'Страницы', 'url' => '/admin', 'route' => 'PagesAdminController', 'glyph' => 'glyphicon-th-list'),
+			),
+		),
+		array(
+			'group' => 'Системные',
+			'items' => array(
+				array('name' => 'Пользователи', 'url' => '/admin/users', 'route' => 'UsersAdminController', 'glyph' => 'glyphicon-user'),
+				array('name' => 'Настройки', 'url' => '/admin/settings', 'route' => 'SettingsAdminController', 'glyph' => 'glyphicon-wrench'),
+			),
+		),
 	);
 
 	$route = Route::currentRouteAction();
--- app/views/admin/widgets/mainMenu/default.blade.php
+++ app/views/admin/widgets/mainMenu/default.blade.php
@@ -1,10 +1,18 @@
 
-<ul class="nav nav-list">
-	@foreach ($menu as $item)
-		@if ($item['route'] == $route)
-			<li class="active"><a href="{{$item['url']}}"><i class="glyphicon {{$item['glyph']}}"></i> {{$item['name']}}</a></li>
-		@else
-			<li><a href="{{$item['url']}}"><i class="glyphicon {{$item['glyph']}}"></i> {{$item['name']}}</a></li>
+<ul class="nav nav-list list-group">
+	@foreach ($menu as $num => $group)
+		@if ($num > 0)
+			<li class="nav-divider"></li>
 		@endif
+		@if (!empty($group['group']))
+			<li class="nav-header">{{ $group['group'] }}</li>
+		@endif
+		@foreach($group['items'] as $item)
+			@if ($item['route'] == $route)
+				<li class="active"><a href="{{$item['url']}}"><i class="glyphicon {{$item['glyph']}}"></i> {{$item['name']}}</a></li>
+			@else
+				<li><a href="{{$item['url']}}"><i class="glyphicon {{$item['glyph']}}"></i> {{$item['name']}}</a></li>
+			@endif
+		@endforeach
 	@endforeach
 </ul>
