81e61f769c9a2d96677f0fda61c720a3a475253c
unlike777 <unlike777@gmail.com>
2015-10-20 13:03:13 +0600

    Сделал лог ежедневным

--- app/start/global.php
+++ app/start/global.php
@@ -37,7 +37,7 @@ ClassLoader::addDirectories(array(
 |
 */
 
-Log::useFiles(storage_path().'/logs/laravel.log');
+Log::useDailyFiles(storage_path().'/logs/laravel.log');
 
 /*
 |--------------------------------------------------------------------------
