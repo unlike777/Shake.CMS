f21446036fc10baced0aeec29bb351b5ffcd356d
unlike777 <unlike777@gmail.com>
2016-01-26 01:29:53 +0300

    Переоформил главную

--- app/views/pages/templates/home.blade.php
+++ app/views/pages/templates/home.blade.php
@@ -2,9 +2,11 @@
 
 @section('content')
 	
-	<h1>{{ $item->title }}</h1>
+	<h1 style="position: fixed; top: 50%; left: 0; width: 100%;  margin-top: -46px; font-size: 28px; font-weight: lighter; font-family: Arial, sans-serif; text-transform: uppercase; letter-spacing: 5px; text-align: center;">
+		Shake.CMS<br>{{ $item->title }}
+	</h1>
 	
-	<div>
+	<div style="display: none;">
 		{{ $item->content }}
 	</div>
 @stop
