b68cd63e5bca95544fc679d9a24a2fc65c63a11e
unlike777 <unlike777@gmail.com>
2016-02-16 02:24:22 +0300

    Migration fix

--- app/database/migrations/2016_02_15_224457_initial.php
+++ app/database/migrations/2016_02_15_224457_initial.php
@@ -18,7 +18,7 @@ class Initial extends Migration {
 			$table->text('text');
 			$table->string('file');
 			$table->string('field');
-			$table->integer('parent_id', false, true);
+			$table->integer('parent_id', false, true)->index();
 			$table->string('parent_type');
 			$table->boolean('is_file');
 			$table->timestamps();
@@ -29,7 +29,7 @@ class Initial extends Migration {
 			$table->increments('id');
 			$table->string('file');
 			$table->string('field');
-			$table->integer('parent_id', false, true);
+			$table->integer('parent_id', false, true)->index();
 			$table->string('parent_type');
 			$table->timestamps();
 		});
@@ -42,9 +42,9 @@ class Initial extends Migration {
 			$table->string('title');
 			$table->text('content');
 			$table->string('template');
-			$table->integer('page_id', false, true);
+			$table->integer('page_id', false, true)->index();
 			$table->boolean('is_home');
-			$table->integer('position', false, true);
+			$table->integer('position', false, true)->index();
 			$table->string('file');
 			$table->timestamps();
 		});
@@ -63,7 +63,7 @@ class Initial extends Migration {
 			$table->string('title');
 			$table->string('keywords');
 			$table->string('description');
-			$table->integer('parent_id', false, true);
+			$table->integer('parent_id', false, true)->index();
 			$table->string('parent_type');
 			$table->timestamps();
 		});
@@ -82,7 +82,7 @@ class Initial extends Migration {
 			$table->boolean('active');
 			$table->string('email')->index();
 			$table->string('password');
-			$table->integer('group', false, true);
+			$table->integer('group', false, true)->index();
 			$table->string('remember_token', 100)->index();
 			$table->timestamps();
 		});
